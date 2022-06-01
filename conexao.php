8420d04babd6533226c1284c24b9555ca740d870
RiicardoAC <rcsilva2405@gmail.com>
2022-06-01 08:29:10 -0500

    Conexão com banco de dados

--- conexao.php
+++ conexao.php
@@ -1,7 +1,13 @@
 <?php
 
+$servidor = "localhost";
+$bdname = "cadastro";
+$user = "root";
+$senha = " ";
 
+$conexao = mysqli_connect($servidor, $user, $senha, $bdname);
 
-
+if(!$conexao)
+    die("Problemas com a conexão com o banco de dados. Descrição do erro: " . mysqli_connect_error());
 
 ?>
