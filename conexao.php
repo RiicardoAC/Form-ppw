e9308a4ffa420eaa847f95ea94ed53bd8fcca420
RiicardoAC <106176120+RiicardoAC@users.noreply.github.com>
2022-06-08 08:08:09 -0500

    Alterando php

--- conexao.php
+++ conexao.php
@@ -1,13 +1,13 @@
 <?php
 
-$servidor = "localhost";
-$bdname = "cadastro";
-$user = "root";
-$senha = " ";
+    $servidor = "localhost";
+    $bdname = "cadastro";
+    $usuario = "root";
+    $senha = "";
 
-$conexao = mysqli_connect($servidor, $user, $senha, $bdname);
+    $conexao = mysqli_connect($servidor, $usuario, $senha, $bdname);
 
-if(!$conexao)
-    die("Problemas com a conexão com o banco de dados. Descrição do erro: " . mysqli_connect_error());
-
+    if(!$conexao)
+      die("Problemas com a conexão com o banco de dados. Descrição do problema: " . mysqli_connect_error());
+    echo "Conexão realizada com sucesso!";
 ?>
--- enviar.php
+++ enviar.php
@@ -1,7 +1,14 @@
 <?php
 
-$POST['nome'] = "nome"
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$contato = $_POST['telefone'];
+$genero = $_POST['genero'];
+$data_nascimento = $_POST['data_nascimento'];
+$qtdIrmaos = $_POST['irmao'];
+foreach ($qtdIrmaos as $val) {
+    $msg .= $val . ", ";
+}
 
-
-
+echo "O nome é: $nome,o email é: $email, o contato é: $contato, o gênero é $genero, a data de nascimento é: $data_nascimento e são $msg irmãos.";
 ?>
