4a5a01fad63c74aa23d6394fdef42897c7aaf499
RiicardoAC <106176120+RiicardoAC@users.noreply.github.com>
2022-06-14 11:46:24 -0500

    Alterações no html

--- enviar.php
+++ enviar.php
@@ -4,6 +4,7 @@
     $contato = $_POST['telefone'];
     $genero = $_POST['genero'];
     $data_nascimento = $_POST['data_nascimento'];
+    $senha = $_POST['senha'];
 
     echo "O nome é: $nome,o email é: $email, o contato é: $contato, o gênero é $genero e a data de nascimento é: $data_nascimento.";
 ?>
