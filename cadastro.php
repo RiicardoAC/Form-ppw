3c62b32e01a0a957196ba205ca860805506c5954
RiicardoAC <106176120+RiicardoAC@users.noreply.github.com>
2022-06-14 12:25:38 -0500

    Ajeitando erro em cadastro.php

--- cadastro.php
+++ cadastro.php
@@ -1,18 +1,18 @@
 <?php
     include('conexao.php');
-    
+
     $nome = $_POST['nome'];
     $email = $_POST['email'];
     $contato = $_POST['telefone'];
     $data_nascimento = $_POST['data_nascimento'];
     $senha = $_POST['senha'];
 
-    $sql = "INSERT INTO  usuarios('nome', 'email', 'contato', 'nascimento') VALUES ('$nome', '$email', '$contato', '$data_nascimento')";
+    $sql = "INSERT INTO usuarios(nome, email, contato, nascimento) VALUES ('$nome', '$email', '$contato', '$data_nascimento')";
 
     if(mysqli_query($conexao, $sql))
         echo 'Cadastrado com sucesso';
     else    
-        echo "Erro". mysqli_connect_error($conexao);
+        echo "Erro!";
 
     mysqli_close($conexao);
 ?>
