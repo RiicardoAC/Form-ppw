ca087a96b3361b4f46182a5af0f3e8e529591f6a
RiicardoAC <106176120+RiicardoAC@users.noreply.github.com>
2022-06-14 11:54:08 -0500

    Deletando campo gênero.

--- enviar.php
+++ enviar.php
@@ -2,9 +2,8 @@
     $nome = $_POST['nome'];
     $email = $_POST['email'];
     $contato = $_POST['telefone'];
-    $genero = $_POST['genero'];
     $data_nascimento = $_POST['data_nascimento'];
     $senha = $_POST['senha'];
 
-    echo "O nome é: $nome,o email é: $email, o contato é: $contato, o gênero é $genero e a data de nascimento é: $data_nascimento.";
+    echo "O nome é: $nome,o email é: $email, o contato é: $contato  e a data de nascimento é: $data_nascimento.";
 ?>
